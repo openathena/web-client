2113899705e74872e84faf77d4c46e9312038103
W Hayden Fuss <wifu1234@gmail.com>
2017-10-12 13:25:15 -0400

    Unwrapping HISTORY events

--- src/sagas/handleSocketEvent.js
+++ src/sagas/handleSocketEvent.js
@@ -1,14 +1,19 @@
 import { eventChannel, buffers } from 'redux-saga';
-import { take, put, call, apply } from 'redux-saga/effects';
+import { take, put, call, all } from 'redux-saga/effects';
 import websocket from '../websocket';
 import wsActions from '../actions/websocket';
+
+const HISTORY = 'HISTORY';
+const EVENT = 'EVENT';
 
 function createSocketChannel(socket) {
   // `eventChannel` takes a subscriber function
   // the subscriber function takes an `emit` argument to put messages onto the channel
   return eventChannel(emit => {
     // setup the subscription
-    socket.onmessage = emit;
+    socket.onmessage = function(socketEvent) {
+      emit(JSON.parse(socketEvent.data));
+    };
 
     socket.onopen = function() {
       socket.send(JSON.stringify({ type: 'OBSERVE' }));
@@ -24,11 +29,16 @@ function createSocketChannel(socket) {
   }, buffers.sliding(10));
 }
 
-function* handleEvent(socketEvent) {
-  // TODO: sort out ws events into meaningful redux actions
-  const payload = yield call(JSON.parse, socketEvent.data);
-  payload.timestamp = socketEvent.timeStamp;
-  yield put(wsActions.incomingMessage(payload));
+function* handleEvent({type, data}) {
+  switch(type) {
+    case HISTORY:
+      yield all(data.map((event) => {
+        return handleEvent(event);
+      }));
+      break;
+    case EVENT:
+      yield put(wsActions.incomingMessage(data));
+  }
 }
 
 export default function*() {
